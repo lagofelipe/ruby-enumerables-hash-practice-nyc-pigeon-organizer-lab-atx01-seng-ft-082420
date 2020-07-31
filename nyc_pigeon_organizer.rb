5885fafdd63e56b06118b2989aeb955e35d258b2
Felipe Dolago <lagofelipe@gmail.com>
2020-07-31 16:10:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require "pry"
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list_by_name = {}
+
+data.each do|attribute_name, attributes|
+  attributes.each do | attribute_value, pigeon_names|
+    pigeon_names.each do |name|
+
+      list_by_name[name] ||= {}
+      list_by_name[name][attribute_name] ||= []
+      list_by_name[name][attribute_name].push(attribute_value.to_s)
+
+    end
+  end
+end
+list_by_name
 end
